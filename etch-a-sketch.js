0ffe8fa050d6c62b222847d8c5a55c29149f9410
fira <firdhair30@gmail.com>
2021-07-29 02:35:01 +0700

    memperbaiki program etch-a-sketch.js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,71 @@
 const container = document.querySelector(".container");
+const black = document.querySelector(".black");
+const warm = document.querySelector(".warm");
+const random = document.querySelector(".random");
 const pix16 = document.querySelector(".grid16");
 const pix32 = document.querySelector(".grid32");
 const pix64 = document.querySelector(".grid64");
-let size = 16;
+
+// let gridCol;
+// let gridRow;
 
 pix16.addEventListener("click", grid16);
 pix32.addEventListener("click", grid32);
 pix64.addEventListener("click", grid64);
 
-for (let i = 1; i <= size; i++) {
-  const gridCol = document.createElement("div");
-  gridCol.classList.add("gridCol");
-  for (let j = 2; j <= size; j++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("gridRow");
-    container.appendChild(gridRow);
-    gridRow.addEventListener("mouseover", function (e) {
-      e.target.style.background = "#b0dab9";
-    });
+auto(16);
+
+function auto(value) {
+  for (let i = 1; i <= value; i++) {
+    const gridCol = document.createElement("div");
+    gridCol.classList.add("gridCol");
+    for (let j = 2; j <= value; j++) {
+      const gridRow = document.createElement("div");
+      gridRow.classList.add("gridRow");
+      container.appendChild(gridRow);
+
+      gridRow.addEventListener("mouseover", playBlack);
+      gridCol.addEventListener("mouseover", playBlack);
+
+      black.addEventListener("click", function (e) {
+        gridRow.addEventListener("mouseover", function (e) {
+          e.target.style.background = "#333";
+        });
+        gridCol.addEventListener("mouseover", function (e) {
+          e.target.style.background = "#333";
+        });
+      });
+      warm.addEventListener("click", function (e) {
+        gridRow.addEventListener("mouseover", function (e) {
+          e.target.style.background = "pink";
+        });
+        gridCol.addEventListener("mouseover", function (e) {
+          e.target.style.background = "pink";
+        });
+      });
+      random.addEventListener("click", function (e) {
+        gridRow.addEventListener("mouseover", function (e) {
+          e.target.style.background = "purple";
+        });
+        gridCol.addEventListener("mouseover", function (e) {
+          e.target.style.background = "purple";
+        });
+      });
+    }
   }
-  container.appendChild(gridCol);
-  gridCol.addEventListener("mouseover", function (e) {
-    e.target.style.background = "#b0dab9";
-  });
+}
+
+function playBlack(e) {
+  e.target.style.background = "#333";
+  console.log("yo");
+}
+
+function playWarm(e) {
+  e.target.style.background = "pink";
+}
+
+function playRandom(e) {
+  e.target.style.background = "purple";
 }
 
 function restart(e) {
@@ -36,72 +79,18 @@ function restart(e) {
 
 function grid16() {
   restart();
-
-  size = 16;
   container.setAttribute("id", "grid16");
-
-  for (let i = 1; i <= size; i++) {
-    const gridCol = document.createElement("div");
-    gridCol.classList.add("gridCol");
-    for (let j = 2; j <= size; j++) {
-      const gridRow = document.createElement("div");
-      gridRow.classList.add("gridRow");
-      container.appendChild(gridRow);
-      gridRow.addEventListener("mouseover", function (e) {
-        e.target.style.background = "#b0dab9";
-      });
-    }
-    container.appendChild(gridCol);
-    gridCol.addEventListener("mouseover", function (e) {
-      e.target.style.background = "#b0dab9";
-    });
-  }
+  auto(16);
 }
 
 function grid32() {
   restart();
-
-  size = 32;
   container.setAttribute("id", "grid32");
-
-  for (let i = 1; i <= size; i++) {
-    const gridCol = document.createElement("div");
-    gridCol.classList.add("gridCol");
-    for (let j = 2; j <= size; j++) {
-      const gridRow = document.createElement("div");
-      gridRow.classList.add("gridRow");
-      container.appendChild(gridRow);
-      gridRow.addEventListener("mouseover", function (e) {
-        e.target.style.background = "#b0dab9";
-      });
-    }
-    container.appendChild(gridCol);
-    gridCol.addEventListener("mouseover", function (e) {
-      e.target.style.background = "#b0dab9";
-    });
-  }
+  auto(32);
 }
 
 function grid64() {
   restart();
-
-  size = 64;
   container.setAttribute("id", "grid64");
-
-  for (let i = 1; i <= size; i++) {
-    const gridCol = document.createElement("div");
-    gridCol.classList.add("gridCol");
-    for (let j = 2; j <= size; j++) {
-      const gridRow = document.createElement("div");
-      gridRow.classList.add("gridRow");
-      container.appendChild(gridRow);
-      gridRow.addEventListener("mouseover", function (e) {
-        e.target.style.background = "#b0dab9";
-      });
-    }
-    container.appendChild(gridCol);
-    gridCol.addEventListener("mouseover", function (e) {
-      e.target.style.background = "#b0dab9";
-    });
-  }
+  auto(64);
 }
