bc5d16ba50fa77c594b4acfde95d408f162b99a4
fira <firdhair30@gmail.com>
2021-07-30 17:36:56 +0700

    created random color feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,19 @@
 const container = document.querySelector(".container");
 const black = document.querySelector(".black");
 const warm = document.querySelector(".warm");
+const span = document.querySelector("span");
 const random = document.querySelector(".random");
 const pix16 = document.querySelector(".grid16");
 const pix32 = document.querySelector(".grid32");
 const pix64 = document.querySelector(".grid64");
 
-// let gridCol;
-// let gridRow;
-
 pix16.addEventListener("click", grid16);
 pix32.addEventListener("click", grid32);
 pix64.addEventListener("click", grid64);
 
-auto(16);
+playGame(16);
 
-function auto(value) {
+function playGame(value) {
   for (let i = 1; i <= value; i++) {
     const gridCol = document.createElement("div");
     gridCol.classList.add("gridCol");
@@ -30,6 +28,7 @@ function auto(value) {
       black.addEventListener("click", function (e) {
         gridRow.addEventListener("mouseover", function (e) {
           e.target.style.background = "#333";
+          span.style.background = "#333";
         });
         gridCol.addEventListener("mouseover", function (e) {
           e.target.style.background = "#333";
@@ -45,7 +44,7 @@ function auto(value) {
       });
       random.addEventListener("click", function (e) {
         gridRow.addEventListener("mouseover", function (e) {
-          e.target.style.background = "purple";
+          e.target.style.background = randomPicker();
         });
         gridCol.addEventListener("mouseover", function (e) {
           e.target.style.background = "purple";
@@ -55,17 +54,40 @@ function auto(value) {
   }
 }
 
+function grid16() {
+  restart();
+  container.setAttribute("id", "grid16");
+  playGame(16);
+}
+
+function grid32() {
+  restart();
+  container.setAttribute("id", "grid32");
+  playGame(32);
+}
+
+function grid64() {
+  restart();
+  container.setAttribute("id", "grid64");
+  playGame(64);
+}
+
+function randomPicker() {
+  let random = [];
+
+  for (let i = 0; i < 3; i++) {
+    let randomChoice = Math.floor(Math.random() * 256);
+    random.push(randomChoice);
+  }
+  let joinChoice = random.join(", ");
+  let rgb = `rgb(${joinChoice})`;
+
+  span.style.background = rgb;
+  return rgb;
+}
+
 function playBlack(e) {
   e.target.style.background = "#333";
-  console.log("yo");
-}
-
-function playWarm(e) {
-  e.target.style.background = "pink";
-}
-
-function playRandom(e) {
-  e.target.style.background = "purple";
 }
 
 function restart(e) {
@@ -75,22 +97,4 @@ function restart(e) {
   document
     .querySelectorAll(".gridRow")
     .forEach((e) => e.parentNode.removeChild(e));
-}
-
-function grid16() {
-  restart();
-  container.setAttribute("id", "grid16");
-  auto(16);
-}
-
-function grid32() {
-  restart();
-  container.setAttribute("id", "grid32");
-  auto(32);
-}
-
-function grid64() {
-  restart();
-  container.setAttribute("id", "grid64");
-  auto(64);
 }
